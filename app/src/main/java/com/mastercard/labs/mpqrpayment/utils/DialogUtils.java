d62f9d05493df7c0749c28ff9a7bdfb606c4ab68
Muhammad Azeem <muhammad.azeem@mastercard.com>
2017-02-03 19:58:05 +0800

    Implement custom dialog header

--- app/src/main/java/com/mastercard/labs/mpqrpayment/utils/DialogUtils.java
+++ app/src/main/java/com/mastercard/labs/mpqrpayment/utils/DialogUtils.java
@@ -3,7 +3,11 @@ package com.mastercard.labs.mpqrpayment.utils;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.annotation.StringRes;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.TextView;
 
 import com.mastercard.labs.mpqrpayment.R;
 
@@ -12,11 +16,20 @@ import com.mastercard.labs.mpqrpayment.R;
  */
 public class DialogUtils {
     public static void showErrorDialog(Context context, @StringRes int title, @StringRes int message) {
-        AlertDialog dialog = new AlertDialog.Builder(context)
-                .setTitle(title)
-                .setMessage(message)
+        TextView messageTextView = new TextView(context);
+        messageTextView.setText(message);
+        messageTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        int padding = context.getResources().getDimensionPixelSize(R.dimen.size_10);
+        float textSize = context.getResources().getDimension(R.dimen.size_x);
+        messageTextView.setPadding(padding, padding, padding, padding);
+        messageTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        messageTextView.setTextColor(ContextCompat.getColor(context, R.color.colorTextMainColor));
+
+        AlertDialog dialog = customAlertDialogBuilder(context)
+                .setView(messageTextView)
                 .setCancelable(true)
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                .setNegativeButton(R.string.dismiss, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
@@ -24,5 +37,13 @@ public class DialogUtils {
                 }).create();
 
         dialog.show();
+    }
+
+    private static AlertDialog.Builder customAlertDialogBuilder(Context context) {
+        TextView titleEditTextView = new TextView(context);
+        titleEditTextView.setBackgroundResource(R.drawable.alert_title_header);
+
+        return new AlertDialog.Builder(context)
+                .setCustomTitle(titleEditTextView);
     }
 }
